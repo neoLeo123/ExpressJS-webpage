96d081907b544c674415e5ec4f7eda5281cc259a
neoLeo123 <113144967+neoLeo123@users.noreply.github.com>
2023-11-15 17:05:35 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*********************************************************************************
- *  WEB322 – Assignment 05** I declare that this assignment is my own work in accordance with Seneca's* 
+ *  WEB322 – Assignment 04** I declare that this assignment is my own work in accordance with Seneca's* 
  * Academic Integrity Policy:** https://www.senecacollege.ca/about/policies/academic-integrity-policy.html**
- *  Name: _______________Leo Ru_______ Student ID: _____144337227_________ Date: ____Nov 15,2023__________** 
+ *  Name: _______________Leo Ru_______ Student ID: _____144337227_________ Date: ____Nov 3,2023__________** 
  * Published URL: __________________________________https://dull-gray-snail-tutu.cyclic.app
 
  * *********************************************************************************/
@@ -13,7 +13,6 @@ const HTTP_PORT = process.env.PORT || 8080;
 const path = require("path");
 const legoData = require("./modules/legoSets");
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const filepath = path.join(__dirname, "views", "home.html");
@@ -24,8 +23,6 @@ app.get("/about", (req, res) => {
   const filepath = path.join(__dirname, "views", "about.html");
   res.render("about");
 });
-
-
 
 app.get("/lego/sets", async (req, res) => {
   try {
@@ -64,67 +61,6 @@ app.get("/lego/sets/:setNum", async (req, res) => {
     res
       .status(404)
       .render("404", { message: "Unable to find requested sets." });
-  }
-});
-
-
-app.get("/lego/addSet", async (req, res) => {
-  try {
-    const themeData = await legoData.getAllThemes();
-    res.render("addSet", { themes: themeData });
-  } catch (error) {
-    console.log(error);
-    // Handle the error appropriately, for example, by rendering an error page
-   // res.status(500).render('error', { message: 'Internal Server Error' });
-  }
-});
-app.post('/lego/addSet', async(req,res)=>{
-  try {
-    const setData = req.body;
-
-    await legoData.addSet(setData);
-    res.redirect('/lego/sets');
-  } catch (err) {
-    
-    console.error(err); 
-    res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
-  }
-})
-
-
-app.get("/lego/editSet/:num", async (req, res) => {
-  try {
-    const setNum = req.params.num;
-
-    const setData = await legoData.getSetByNum(setNum);
-    const themeData = await legoData.getAllThemes();
-    res.render("editSet", { themes: themeData, set: setData });
-  } catch (error) {
-    res.status(404).render("404", { message: "Cannot retrieve set" });
-  }
-});
-app.post('/lego/editSet', async(req,res)=>{
-  try {
-    const setnum = req.body.set_num;
-    const data = req.body;
-    await legoData.editSet(setnum,data);
-    res.redirect('/lego/sets');
-  } catch (err) {
-    
-    console.error(err); 
-    res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
-  }
-})
-
-app.get("/lego/deleteSet/:num", async (req, res) => {
-  try {
-    const setNum = req.params.num;
-
-    await legoData.deleteSet(setNum);
-  
-    res.redirect('/lego/sets');
-  } catch (err) {
-    res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` });
   }
 });
 
